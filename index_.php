e0317ab8cc9cbc29e01f71dceeeeab7af7e2382b
kevin <kevin.maton@condorcet.be>
2021-03-17 16:41:27 +0100

    commit de l index

--- index_.php
+++ index_.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <?php
 //index public
@@ -11,66 +10,72 @@ $cnx = Connexion::getInstance($dsn, $user, $password);
 ?>
 
 <html>
-    <head>
-        <title>Project web Kevin Maton</title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        <link rel="stylesheet" href="./admin/lib/css/style.css"/>
-        <link rel="stylesheet" href="./admin/lib/css/custom.css"/>
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
-        <link rel="stylesheet" href="./admin/lib/css/style_pc.css"/>
-        <link rel="stylesheet" href="./admin/lib/css/mediaqueries.css"/>
-        <link rel="stylesheet" href="./admin/lib/css/styleBt.css"/>
-        <link rel="stylesheet" href="./admin/lib/css/custom.css"/>
-    </head>
+<head>
+    <title>Project web Kevin Maton</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+            integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+          integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="./admin/lib/css/style.css"/>
+    <link rel="stylesheet" href="./admin/lib/css/custom.css"/>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+          integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="./admin/lib/css/style_pc.css"/>
+    <link rel="stylesheet" href="./admin/lib/css/mediaqueries.css"/>
+    <link rel="stylesheet" href="./admin/lib/css/styleBt.css"/>
+    <link rel="stylesheet" href="./admin/lib/css/custom.css"/>
+</head>
 
-    <body class="bg-dark">
-        <div id="page">
-            <header class="img_header">
+<body class="bg-dark">
+<div id="page">
+    <header class="img_header">
 
-            </header>
-            <section id="colGauche">
-                <nav>
-                    <?php
-                    $path = "./lib/php/public_menu.php";
-                    if (file_exists($path)) {
-                        include ($path);
-                    }
-                    ?>
-                </nav>
-
-            </section>
-            <section class="center-block">
-                <div id="main">
-                    <?php
-                    if (!isset($_SESSION['page'])) {
-                        $_SESSION['page'] = "accueil.php";
-                    }
-                    if (isset($_GET['page'])) {
-                        $_SESSION['page'] = $_GET['page'];
-                    }
-                    $path = "./pages/" . $_SESSION['page'];
-
-                    // print "path: " . $path . "<br>";
-                    if (file_exists($path)) {
-                        include ($path);
-                    } else {
-                        include ("./pages/page404.php");
-                    }
-                    ?>
-                </div>
-            </section>
-
-        </div>
+    </header>
+    <section id="colGauche">
         <nav>
             <?php
-                $path="./lib/php/public_footer.php";
-                if (file_exists($path)) {
-                include ($path);
-             }
+            $path = "./lib/php/public_menu.php";
+            if (file_exists($path)) {
+                include($path);
+            }
             ?>
         </nav>
-    </body>
+
+    </section>
+    <section class="center-block">
+        <div id="main">
+            <?php
+            if (!isset($_SESSION['page'])) {
+                $_SESSION['page'] = "accueil.php";
+            }
+            if (isset($_GET['page'])) {
+                $_SESSION['page'] = $_GET['page'];
+            }
+            $path = "./pages/" . $_SESSION['page'];
+
+            // print "path: " . $path . "<br>";
+            if (file_exists($path)) {
+                include($path);
+            } else {
+                include("./pages/page404.php");
+            }
+            ?>
+        </div>
+    </section>
+
+</div>
+<nav>
+    <?php
+    $path = "./lib/php/public_footer.php";
+    if (file_exists($path)) {
+        include($path);
+    }
+    ?>
+</nav>
+</body>
 </html>
